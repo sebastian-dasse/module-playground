d0acff4b73ae1cc10d819325438b39988766b0fe
sebastian-dasse <sebastian.dasse@mailbox.org>
2019-05-19 13:09:14 +0200

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # What is this project all about?
 
-I just wanted to get a first understanding of how the **Java Module System** and the Java **ServiceLoader** work and how they play together in a gradle project. Also I wanted to try out Java's **Dynamic Proxy** in practice, which I did in the alternative service implementation with added logging.
+I just wanted to get a first understanding of how the **Java Module System** and the Java **ServiceLoader** work and how they play together in a gradle project. Also I wanted to try out Java's **Dynamic Proxy** in practice, which I did in the alternative service implementations.
--- foo-impl/src/main/java/de/sebdas/foo/impl/DynamicProxy.java
+++ foo-impl/src/main/java/de/sebdas/foo/impl/DynamicProxy.java
@@ -3,7 +3,8 @@ package de.sebdas.foo.impl;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
-class DynamicProxy {
+final class DynamicProxy {
+  private DynamicProxy() {}
 
   static <T, I extends T> T proxyWithLogging(final Class<T> tClass, final I tImpl) {
     return proxy(tClass, new LoggingInvocationHandler(tImpl));
@@ -11,7 +12,6 @@ class DynamicProxy {
 
   static <T, I extends T> T proxyWithDelay(final Class<T> tClass, final I tImpl, final long delayMillis) {
     return proxy(tClass, new DelayingInvocationHandler(tImpl, delayMillis));
-
   }
 
   private static <T> T proxy(final Class<T> tClass, final InvocationHandler invocationHandler) {
